7077ebe9a12a207d09c1fdf8a92a6f3421e23e0e
Neon <neonmed571@gmail.com>
2024-11-16 23:16:10 +0200

    task one

---
+++ main.py
@@ -0,0 +1,9 @@
+# Task 1
+
+rightPassword = "password123"
+userPassword = input("Введіть ваш пароль: ")
+
+if rightPassword == userPassword:
+    print("Ви увійшли в систему")
+else:
+    print("Неправильний пароль")
