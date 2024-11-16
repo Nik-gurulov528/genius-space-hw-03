5e68f687e4677b737e9b9cbf747b82150eef1893
Neon <neonmed571@gmail.com>
2024-11-16 23:23:59 +0200

    task two

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Умовні конструкції
 # Task 1
 
 rightPassword = "password123"
@@ -7,3 +8,24 @@ if rightPassword == userPassword:
     print("Ви увійшли в систему")
 else:
     print("Неправильний пароль")
+
+# Task 2
+
+dayNumber = int(input("Choose number from 1 to 7: "))
+if dayNumber < 1 or dayNumber > 7:
+    print("Error, invalid data")
+else:
+    if dayNumber == 1:
+        print("Monday")
+    elif dayNumber == 2:
+        print("Tuesday")
+    elif dayNumber == 3:
+        print("Wednesday")
+    elif dayNumber == 4:
+        print("Tursday")
+    elif dayNumber == 5:
+        print("Friday")
+    elif dayNumber == 6:
+        print("Saturday")
+    else:
+        print("Sunday")
